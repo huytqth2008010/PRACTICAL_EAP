09a9d12dfb887e56fbefee891665ab93a462cba6
Huy Quang <truongquanghuybx0914@gmail.com>
2021-12-13 09:55:52 +0700

    Add project files.

---
+++ PRACTICAL_EAP/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PRACTICAL_EAP.Models
+{
+    public partial class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
